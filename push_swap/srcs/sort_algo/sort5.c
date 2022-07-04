63108fc66e23df383766428c1bc02ffd27ff4665
Nicolas Grenoux <ngrenoux@3C1.42angouleme.fr>
2022-07-04 16:14:45 +0200

    JEANNE OSCOUR

--- push_swap/srcs/sort_algo/sort5.c
+++ push_swap/srcs/sort_algo/sort5.c
@@ -6,7 +6,7 @@
 /*   By: ngrenoux <ngrenoux@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/04 10:30:40 by ngrenoux          #+#    #+#             */
-/*   Updated: 2022/07/04 14:26:22 by ngrenoux         ###   ########.fr       */
+/*   Updated: 2022/07/04 16:14:05 by ngrenoux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 void sort5(t_stack *lst)
 {
-	
+	t_stack *stack_b;
+
+	stack_b = NULL;
+	if (check_if_first_is_min(lst) == 1)
+	{
+		push(lst, stack_b);
+	}
 }
