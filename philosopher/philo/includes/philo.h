1a54635c050f65fa13b5d16196a540d1ade04b7e
Nicolas Grenoux <ngrenoux@1F2.42angouleme.fr>
2022-10-25 15:52:57 +0200

    struct

--- philosopher/philo/includes/philo.h
+++ philosopher/philo/includes/philo.h
@@ -6,7 +6,7 @@
 /*   By: ngrenoux <ngrenoux@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 09:34:38 by ngrenoux          #+#    #+#             */
-/*   Updated: 2022/10/24 15:50:01 by ngrenoux         ###   ########.fr       */
+/*   Updated: 2022/10/25 15:18:19 by ngrenoux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,35 @@
 # include <sys/time.h>
 # include <pthread.h>
 
+# define TRUE 1
+# define FALSE 0
+# define SUCCESS 1
+# define ERROR 0
+
+typedef struct s_philo
+{
+	int				nb;
+	int				nb_eat;
+	int				time_to_eat;
+	int				time_to_die;
+	int				time_to_sleep;
+	long int		eat_time;
+	struct t_data	*data;
+	pthread_t		thread;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
+}	t_philo;
+
 typedef struct s_data
 {
-	pthread_t		philo;
-	pthread_mutex_t	fork;
-	long int		time_to_eat;
-	long int		time_to_die;
-	long int		time_to_sleep;
 	int				nb_philo;
-	int				nb_philo_eat;
+	int				is_dead;
+	struct timeval	time;
+	t_philo			*philo;
+	pthread_mutex_t	*fork;
+	pthread_mutex_t	mutex;
+	pthread_mutex_t	*left_fork;
+	pthread_mutex_t	*right_fork;
 }	t_data;
 
 void	ft_init(t_data *data);
--- philosopher/philo/srcs/main.c
+++ philosopher/philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: ngrenoux <ngrenoux@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 11:02:17 by ngrenoux          #+#    #+#             */
-/*   Updated: 2022/10/24 15:51:18 by ngrenoux         ###   ########.fr       */
+/*   Updated: 2022/10/25 12:25:36 by ngrenoux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,5 @@
 
 int	main(int ac, char **av)
 {
-	t_data *data;
-	
-	ft_init(data);
 	
 }
