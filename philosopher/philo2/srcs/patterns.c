53ebb15dae0b0a83be3e2b120841e303c9c30557
Fenbjorn <ngrenoux@gmail.com>
2022-11-02 21:53:55 +0100

    need free

--- philosopher/philo2/srcs/patterns.c
+++ philosopher/philo2/srcs/patterns.c
@@ -6,35 +6,35 @@
 /*   By: ngrenoux <ngrenoux@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/24 13:05:33 by ngrenoux          #+#    #+#             */
-/*   Updated: 2022/10/27 12:46:27 by ngrenoux         ###   ########.fr       */
+/*   Updated: 2022/11/02 21:43:21 by ngrenoux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-static void	ft_eating(t_philo *philo)
+static void ft_eating(t_philo *philo)
 {
-	t_data	*data;
+	t_data *data;
 
 	data = philo->data;
-	pthread_mutex_lock(&data->fork[philo->left_fork]);
+	pthread_mutex_lock(&(data->fork[philo->left_fork]));
 	ft_actions_messages(data, philo->id, "has taken a fork");
-	pthread_mutex_lock(&data->fork[philo->right_fork]);
+	pthread_mutex_lock(&(data->fork[philo->right_fork]));
 	ft_actions_messages(data, philo->id, "has taken a fork");
-	pthread_mutex_lock(&data->check_meal);
+	pthread_mutex_lock(&(data->check_meal));
 	ft_actions_messages(data, philo->id, "is eating");
 	philo->time_last_meal = ft_get_time();
-	pthread_mutex_unlock(&data->check_meal);
+	pthread_mutex_unlock(&(data->check_meal));
 	ft_sleep_smartly(data, data->time_to_eat);
 	philo->nb_ate++;
-	pthread_mutex_unlock(&data->fork[left_fork]);
-	pthread_mutex_unlock(&data->fork[right_fork]);
+	pthread_mutex_unlock(&(data->fork[philo->left_fork]));
+	pthread_mutex_unlock(&(data->fork[philo->right_fork]));
 }
 
-static void	*ft_patterns(void *args)
+static void *ft_patterns(void *args)
 {
-	t_philo	*philo;
-	t_data	*data;
+	t_philo *philo;
+	t_data *data;
 
 	philo = (t_philo *)args;
 	data = philo->data;
@@ -44,7 +44,7 @@ static void	*ft_patterns(void *args)
 	{
 		ft_eating(philo);
 		if (data->ate_goal)
-			break ;
+			break;
 		ft_actions_messages(data, philo->id, "is sleeping");
 		ft_sleep_smartly(data, data->time_to_sleep);
 		ft_actions_messages(data, philo->id, "is thinking");
@@ -52,9 +52,9 @@ static void	*ft_patterns(void *args)
 	return (NULL);
 }
 
-static void	ft_exit(t_data *data, t_philo *philo)
+static void ft_exit(t_data *data, t_philo *philo)
 {
-	int	i;
+	int i;
 
 	i = 0;
 	while (i < data->nb_philo)
@@ -65,15 +65,15 @@ static void	ft_exit(t_data *data, t_philo *philo)
 	i = 0;
 	while (i < data->nb_philo)
 	{
-		pthread__mutex_destroy(&data->fork[i]);
+		pthread_mutex_destroy(&data->fork[i]);
 		i++;
 	}
-	pthread__mutex_destroy(&data->writing);
+	pthread_mutex_destroy(&data->writing);
 }
 
-static void	ft_check_if_is_dead(t_data *data, t_philo *philo)
+static void ft_check_if_is_dead(t_data *data, t_philo *philo)
 {
-	int	i;
+	int i;
 
 	while (!data->ate_goal)
 	{
@@ -81,37 +81,36 @@ static void	ft_check_if_is_dead(t_data *data, t_philo *philo)
 		while (i < data->nb_philo && !data->is_dead)
 		{
 			pthread_mutex_lock(&data->check_meal);
-			if((philo[i].time_last_meal - ft_get_time()) > data->time_to_die)
+			if ((philo[i].time_last_meal - ft_get_time()) > data->time_to_die)
 			{
 				ft_actions_messages(data, i, "died");
 				data->is_dead = 1;
 			}
-			pthread_mutex_unlock(data->check_meal);
+			pthread_mutex_unlock(&data->check_meal);
 			usleep(100);
 			i++;
 		}
 		if (data->is_dead)
-			break ;
+			break;
 		i = 0;
-		while (data->nb_eat != -1 && i < data->nb_philo
-			&& philo[i].nb_ate >= data->nb_eat)
+		while (data->nb_eat != -1 && i < data->nb_philo && philo[i].nb_ate >= data->nb_eat)
 			i++;
 		if (i == data->nb_philo)
 			data->ate_goal = 1;
 	}
 }
 
-int	ft_begin(t_data *data)
+int ft_begin(t_data *data)
 {
-	int		i;
-	t_philo	*philo;
+	int i;
+	t_philo *philo;
 
 	i = 0;
 	philo = data->philo;
 	data->starting_time = ft_get_time();
 	while (i < data->nb_philo)
 	{
-		if (pthread_create(&philo[i].thread), NULL, &ft_patterns, &philo[i])
+		if (pthread_create(&philo[i].thread, NULL, &ft_patterns, &philo[i]))
 			return (1);
 		philo[i].time_last_meal = ft_get_time();
 		i++;
