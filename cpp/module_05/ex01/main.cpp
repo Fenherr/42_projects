5d297313814be943e6c56d35df813ebfb50b2d04
ngrenoux <ngrenoux@gmail.com>
2023-03-27 09:59:43 +0200

    fix test

--- cpp/module_05/ex01/main.cpp
+++ cpp/module_05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ngrenoux <ngrenoux@student.42angouleme.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/23 13:01:10 by ngrenoux          #+#    #+#             */
-/*   Updated: 2023/03/24 10:45:45 by ngrenoux         ###   ########.fr       */
+/*   Updated: 2023/03/27 09:58:17 by ngrenoux         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,39 +15,77 @@
 
 int main()
 {
-	try
 	{
-		Bureaucrat employee("Employee", 110);
-		
-		Form form("Test form", 100, 100);
-		
-		std::cout << std::endl;
-		
-		std::cout << employee << std::endl;
-		
-		std::cout << std::endl;
-		
-		std::cout << form << std::endl;
-		
-		std::cout << std::endl;
-		
-		employee.signForm(form);
-		
-		std::cout << std::endl;
-		
-		std::cout << form << std::endl;
+		try
+		{
+			Bureaucrat employee("Employee", 110);
+			
+			Form form("Test form", 100, 100);
+			
+			std::cout << std::endl;
+			
+			std::cout << employee << std::endl;
+			
+			std::cout << std::endl;
+			
+			std::cout << form << std::endl;
+			
+			std::cout << std::endl;
+			
+			employee.signForm(form);
+			
+			std::cout << std::endl;
+			
+			std::cout << form << std::endl;
+		}
+		catch(Bureaucrat::GradeTooHighException &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		catch(Bureaucrat::GradeTooLowException &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 	}
-	catch(Bureaucrat::GradeTooHighException &e)
+	std::cout << std::endl;
 	{
-		std::cout << e.what() << std::endl;
+		try
+		{
+			Bureaucrat employee("Employee", 100);
+			
+			Form form("Test form", 100, 100);
+			
+			std::cout << std::endl;
+			
+			std::cout << employee << std::endl;
+			
+			std::cout << std::endl;
+			
+			std::cout << form << std::endl;
+			
+			std::cout << std::endl;
+			
+			employee.signForm(form);
+			
+			std::cout << std::endl;
+			
+			std::cout << form << std::endl;
+		}
+		catch(Bureaucrat::GradeTooHighException &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		catch(Bureaucrat::GradeTooLowException &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 	}
-	catch(Bureaucrat::GradeTooLowException &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	catch(const std::exception& e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	
 }
