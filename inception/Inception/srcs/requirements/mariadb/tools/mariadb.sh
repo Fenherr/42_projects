141087315cdffc7d235ea731b00279ee20312302
Fenbjorn <61088316+Fenbjorn@users.noreply.github.com>
2023-05-17 09:45:49 +0200

    Add files via upload

---
+++ inception/Inception/srcs/requirements/mariadb/tools/mariadb.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+mysql_install_db
+
+/etc/init.d/mysql start
+
+if [-d "/var/lib/mysql/$MYSQL_DATABASE"]
+then
+	echo "Database already exist"
+else
+	mysql_secure_installation << EOF
+
+y
+1234
+1234
+y
+n
+y
+y
+EOF
+
+echo "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD'; FLUSH PRIVILEGES;" | mysql -uroot
+
+echo "CREATE DATABASE IF NOT EXIST $MYSQL_DATABASE; GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD'; FLUSH PRIVILEGES" | mysql -uroot
+
+mysql -uroot -p$MYSQL_ROOT_PASSWORD $MYSQL_DATABASE < /usr/local/bin/wordpress.sql
+
+fi
+
+/etc/init.d/mysql stop
+
+exec "$@"
